7bfbaa0a38b858c420b92d6be1905d59694bd088
R3ll36 <rus.cosmin@icloud.com>
2023-09-27 10:56:57 +0200

    New Gsap

--- quart.js
+++ quart.js
@@ -1,265 +1,82 @@
 //<script src="http://localhost:3000/quart.js"></script>
 //<script src="https://cdn.jsdelivr.net/gh/R3ll36/JSServ@main/quart.js"></script>
 
-// Function to load and play the Lottie animation
-function playLottieAnimation() {
-  // Load the Lottie animation
-  const animationContainer = document.getElementById('lottie-container');
-  const animationData = {
-    container: animationContainer,
-    renderer: 'svg',
-    loop: false, // Set to false to play the animation once
-    autoplay: false, // Set autoplay to false initially
-    path: 'https://uploads-ssl.webflow.com/64339668e5bde96984e41146/64eef1a47046f4b5858347c0_SP-CE_LogoWebsite-v3.json', // Replace with your animation's URL
-  };
-  const anim = lottie.loadAnimation(animationData);
+$('.nav_wrap').each(function () {
+  let hamburgerEl = $(this).find('.nav_hamburger_wrap');
+  let navLineEl = $(this).find('.nav_hamburger_line');
+  let menuContainEl = $(this).find('.menu_contain');
+  let flipItemEl = $(this).find('.nav_hamburger_base');
+  let menuWrapEl = $(this).find('.menu_wrap');
+  let menuBaseEl = $(this).find('.menu_base');
+  let menuLinkEl = $(this).find('.menu_link.is--dark');
 
-  anim.addEventListener('DOMLoaded', () => {
-    // Set the initial frame to start from 20%
-    anim.goToAndStop(anim.totalFrames * 0.3, true);
+  let flipDuration = 0.6;
 
-    // Function to replay from 20% when the animation completes
-    function replayFrom20Percent() {
-      anim.goToAndPlay(anim.totalFrames * 0.7); // Go to 20% and play
+  function flip(forwards) {
+    let state = Flip.getState(flipItemEl);
+    if (forwards) {
+      flipItemEl.appendTo(menuContainEl);
+    } else {
+      flipItemEl.appendTo(hamburgerEl);
     }
-
-    // Start the animation
-    anim.play();
-
-    // Add an event listener for when the animation completes
-    anim.addEventListener('complete', () => {
-      replayFrom20Percent(); // Replay from 20% when complete
-    });
-  });
-}
-
-// Call the function to start the animation
-playLottieAnimation();
-
-function slider1() {
-  let splides = $('.slider1');
-
-  for (let i = 0, splideLength = splides.length; i < splideLength; i++) {
-    new Splide(splides[i], {
-      // Your existing options...
-      perPage: 1,
-      perMove: 1,
-      focus: 0,
-      type: 'loop',
-      gap: '1rem',
-      arrows: false,
-      pagination: false,
-      speed: 600,
-      dragAngleThreshold: 30,
-      autoWidth: false,
-      rewind: false,
-      rewindSpeed: 400,
-      waitForTransition: false,
-      updateOnMove: true,
-      trimSpace: false,
-      breakpoints: {
-        991: {
-          perPage: 1,
-        },
-        767: {
-          perPage: 1,
-        },
-        479: {
-          perPage: 1,
-        },
-      },
-      autoplay: true, // Enable autoplay with default settings
-      // If you want custom autoplay settings, use an object like this:
-      autoplay: {
-        delay: 3000, // Time between each slide transition in milliseconds
-        disableOnInteraction: true, // Set to true to stop autoplay on user interaction (e.g., when the user manually changes slides)
-      },
-    }).mount();
-  }
-}
-
-slider1();
-
-//COOKIES
-function setCookie(name, value, days) {
-  var expires = '';
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = '; expires=' + date.toUTCString();
-  }
-  document.cookie = name + '=' + (value || '') + expires + '; path=/';
-}
-
-function getCookie(name) {
-  var nameEQ = name + '=';
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
-document.addEventListener('DOMContentLoaded', function (event) {
-  var cookieConsent = document.getElementById('cookie-consent');
-  var cookieConsentOk = document.getElementById('cookie-consent-ok');
-
-  if (getCookie('cookieConsentShown')) {
-    cookieConsent.style.display = 'none';
-  } else {
-    cookieConsent.style.display = 'flex';
+    Flip.from(state, { duration: flipDuration });
   }
 
-  cookieConsentOk.addEventListener('click', function () {
-    setCookie('cookieConsentShown', true, 30);
-    cookieConsent.style.display = 'none';
-  });
-});
-
-//LENIS SCROLL
-
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  direction: 'vertical', // vertical, horizontal
-  gestureDirection: 'vertical', // vertical, horizontal, both
-  smooth: true,
-  mouseMultiplier: 1,
-  smoothTouch: false,
-  touchMultiplier: 2,
-  infinite: false,
-});
-
-// Event listeners to control Lenis behavior
-
-$('[data-lenis-start]').on('click', function () {
-  lenis.start();
-});
-
-$('[data-lenis-stop]').on('click', function () {
-  lenis.stop();
-});
-
-$('[data-lenis-toggle]').on('click', function () {
-  $(this).toggleClass('stop-scroll');
-  if ($(this).hasClass('stop-scroll')) {
-    lenis.stop();
-  } else {
-    lenis.start();
-  }
-});
-
-// The raf function for smooth scrolling
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
-
-// SWIPER SLIDER
-
-$(document).ready(function () {
-  // Function to check if the device is mobile
-  function isMobileDevice() {
-    return window.matchMedia('(max-width: 500px)').matches;
-  }
-
-  var slidesToShow = isMobileDevice() ? 1 : 'auto'; // Set '1' for mobile, 'auto' otherwise
-
-  var slider_wrapper = new Swiper('.slider-wrapper', {
-    wrapperClass: 'slider-list',
-    slideClass: 'slider-item',
-    navigation: {
-      nextEl: '.next-slide',
-      prevEl: '.prev-slide',
-    },
-    pagination: {
-      type: 'bullets',
-      el: '.pagination',
-      clickable: true,
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    speed: 700,
-    slidesPerView: slidesToShow, // Use the slidesToShow variable
-    loop: true,
-
-    on: {
-      init: function () {
-        var swiper_pagination_bulletwe_style = document.createElement('style');
-        swiper_pagination_bulletwe_style.type = 'text/css';
-        swiper_pagination_bulletwe_style.innerHTML = ` .swiper-pagination-bullet{   background:#fff;
-    margin-right:8px;
-    transition:.2s;
-   } `;
-        document
-          .getElementsByTagName('head')[0]
-          .appendChild(swiper_pagination_bulletwe_style);
-        var swiper_pagination_bullethoverwe_style =
-          document.createElement('style');
-        swiper_pagination_bullethoverwe_style.type = 'text/css';
-        swiper_pagination_bullethoverwe_style.innerHTML = ` .swiper-pagination-bullet:hover{   opacity:.7;
-   } `;
-        document
-          .getElementsByTagName('head')[0]
-          .appendChild(swiper_pagination_bullethoverwe_style);
-        var swiper_pagination_bullet_activehoverwe_style =
-          document.createElement('style');
-        swiper_pagination_bullet_activehoverwe_style.type = 'text/css';
-        swiper_pagination_bullet_activehoverwe_style.innerHTML = ` .swiper-pagination-bullet-active:hover{   opacity:1;
-   } `;
-        document
-          .getElementsByTagName('head')[0]
-          .appendChild(swiper_pagination_bullet_activehoverwe_style);
-        var swiper_pagination_fractionwe_style =
-          document.createElement('style');
-        swiper_pagination_fractionwe_style.type = 'text/css';
-        swiper_pagination_fractionwe_style.innerHTML = ` .swiper-pagination-fraction{   font-size:16px;
-   } `;
-        document
-          .getElementsByTagName('head')[0]
-          .appendChild(swiper_pagination_fractionwe_style);
-        var slide_imagewe_style = document.createElement('style');
-        slide_imagewe_style.type = 'text/css';
-        slide_imagewe_style.innerHTML = ` .slide-image{   transition:transform .2s;
-   } `;
-        document
-          .getElementsByTagName('head')[0]
-          .appendChild(slide_imagewe_style);
-        var slide_contentwe_style = document.createElement('style');
-        slide_contentwe_style.type = 'text/css';
-        slide_contentwe_style.innerHTML = ` .slide-content{   transition:opacity .2s;
-   } `;
-        document
-          .getElementsByTagName('head')[0]
-          .appendChild(slide_contentwe_style);
-        $('.slider-item').on('mouseover', function () {
-          $(this).find('.slide-image').css({
-            transform: 'scale(1)',
-          });
-          $(this).find('.slide-content').css({
-            opacity: '.8',
-          });
-        });
-        $('.slider-item').on('mouseout', function () {
-          $(this).find('.slide-image').css({
-            transform: 'scale(1)',
-          });
-          $(this).find('.slide-content').css({
-            opacity: '1',
-          });
-        });
-      },
+  let tl = gsap.timeline({ paused: true });
+  tl.set(menuWrapEl, { display: 'flex' });
+  tl.from(menuBaseEl, {
+    opacity: 0,
+    duration: flipDuration,
+    ease: 'none',
+    onStart: () => {
+      flip(true);
     },
   });
-});
+  tl.to(navLineEl.eq(0), { y: 4, rotate: 45, duration: flipDuration }, '<');
+  tl.to(navLineEl.eq(1), { y: -4, rotate: -45, duration: flipDuration }, '<');
+  tl.from(menuLinkEl, {
+    opacity: 0,
+    yPercent: 50,
+    duration: 0.2,
+    stagger: { amount: 0.2 },
+    onReverseComplete: () => {
+      flip(false);
+    },
+  });
 
-//TEXT ANIMATION
+  function openMenu(open) {
+    if (!tl.isActive()) {
+      if (open) {
+        tl.play();
+        hamburgerEl.addClass('nav-open');
+      } else {
+        tl.reverse();
+        hamburgerEl.removeClass('nav-open');
+      }
+    }
+  }
+
+  hamburgerEl.on('click', function () {
+    if ($(this).hasClass('nav-open')) {
+      openMenu(false);
+    } else {
+      openMenu(true);
+    }
+  });
+
+  menuBaseEl.on('mouseenter', function () {
+    openMenu(false);
+  });
+  menuBaseEl.on('click', function () {
+    openMenu(false);
+  });
+
+  $(document).on('keydown', function (e) {
+    if (e.key === 'Escape') {
+      openMenu(false);
+    }
+  });
+});
 
 window.addEventListener('DOMContentLoaded', (event) => {
   setTimeout(() => {
@@ -283,7 +100,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
             trigger: textEl,
             start: 'top bottom',
             end: 'bottom bottom',
-            toggleActions: 'none play none reset',
+            toggleActions: 'none play none none',
           },
         });
         tl.fromTo(
@@ -311,75 +128,102 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }, 700);
 });
 
-//VIDEO
 
-document.addEventListener('DOMContentLoaded', function () {
-  const playPauseButtons = document.querySelectorAll('.play-pause');
+let splitType = new SplitType("[hoverstagger='text']", {
+  types: "words,chars",
+  tagName: "span"
+});
 
-  function toggleVideoPlayback(video, button) {
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
+$("[hoverstagger='link']").each(function (index) {
+  let text1 = $(this).find("[hoverstagger='text']").eq(0);
+  let text2 = $(this).find("[hoverstagger='text']").eq(1);
+
+  let tl = gsap.timeline({
+    paused: true,
+    defaults: {
+      duration: 0.5,
+      ease: "power2.out"
     }
-  }
+  });
+  tl.fromTo(text1.find(".char:nth-child(odd)"), { yPercent: 100 }, { yPercent: 0 });
+  tl.fromTo(text2.find(".char:nth-child(odd)"), { yPercent: 0 }, { yPercent: -100 }, 0);
+  tl.fromTo(text1.find(".char:nth-child(even)"), { yPercent: 0 }, { yPercent: 100 }, 0);
+  tl.fromTo(text2.find(".char:nth-child(even)"), { yPercent: -100 }, { yPercent: 0 }, 0);
 
-  playPauseButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-      const videoId = this.getAttribute('data-video');
-      const video = document.querySelector(`video[data-video="${videoId}"]`);
-      if (video) {
-        toggleVideoPlayback(video, button);
-      } else {
-        console.log(`No video found with data-video="${videoId}"`);
+  $(this).on("mouseenter", function () {
+    tl.restart();
+  });
+});
+
+
+
+let typeSplit;
+// Split the text up
+function runSplit() {
+  typeSplit = new SplitType(".split-lines", {
+    types: "lines, words"
+  });
+  $(".line").append("<div class='line-mask'></div>");
+  createAnimation();
+}
+runSplit();
+// Update on window resize
+let windowWidth = $(window).innerWidth();
+window.addEventListener("resize", function () {
+  if (windowWidth !== $(window).innerWidth()) {
+    windowWidth = $(window).innerWidth();
+    typeSplit.revert();
+    runSplit();
+  }
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+function createAnimation() {
+  $(".line").each(function (index) {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: $(this),
+        // trigger element - viewport
+        start: "top center",
+        end: "bottom center",
+        scrub: 1
       }
     });
+    tl.to($(this).find(".line-mask"), {
+      width: "0%",
+      duration: 1
+    });
   });
+}
 
-  const videos = document.querySelectorAll('[wp-embed="video"]');
+// Make sure to include the ScrollTrigger plugin before using it
+gsap.registerPlugin(ScrollTrigger);
 
-  function handleVideoVisibility(video, isVisible) {
-    if (isVisible && video.paused) {
-      video.play();
-    } else if (!isVisible && !video.paused) {
-      video.pause();
-    }
-  }
+// Use the ScrollTrigger to trigger animations when elements come into view
+$(document).ready(function () {
+  $('.html').each(function (index) {
+    let targetElement = $(this);
 
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        const video = entry.target;
-        const isVisible = entry.isIntersecting;
-        handleVideoVisibility(video, isVisible);
-      });
-    },
-    {
-      threshold: 0.2,
-    }
-  );
-
-  videos.forEach((video) => {
-    if (video.tagName === 'VIDEO') {
-      video.pause();
-      observer.observe(video);
-    }
-  });
-
-  // Progress bar functionality
-  playPauseButtons.forEach(function (button) {
-    const videoId = button.getAttribute('data-video');
-    const video = document.querySelector(`video[data-video="${videoId}"]`);
-    const progressContainer = button.parentElement.querySelector(
-      '.progress-container'
+    gsap.fromTo(
+      targetElement,
+      {
+        y: '110%',
+        opacity: 0,
+      },
+      {
+        y: '0%',
+        opacity: 1,
+        duration: 0.3,
+        delay: index * 0.2,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: targetElement, // Element to trigger the animation
+          start: 'top 110%', // Start the animation when 80% of the element is in view
+          end: 'center center', // End the animation when the element is at the center of the viewport
+          toggleActions: 'play none none none', // Play the animation when scrolling into view
+        },
+      }
     );
-    const progressBar = progressContainer.querySelector('.progress-bar');
-
-    if (video) {
-      video.addEventListener('timeupdate', function () {
-        const progress = (video.currentTime / video.duration) * 100 + '%';
-        progressBar.style.width = progress;
-      });
-    }
   });
 });
